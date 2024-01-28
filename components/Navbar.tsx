9aa1d2ae2942b5d9a94886ac26bb52708f390193
Bima Pn <bimaptr12@gmail.com>
2024-01-28 19:16:01 +0700

    Add transition in about component landing page.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,21 +2,20 @@
 import Link from "next/link"
 import { useEffect, useState } from "react";
 import { IoTicketOutline } from "react-icons/io5"
+import { motion } from "framer-motion"
 
 const windowPosition = window.pageYOffset
 
-const Navbar = ({isLanding=false}:{isLanding?:boolean}) => {
-  const [isNavbarVisible, setNavbarVisible] = useState(false);
+const LandingNavbar = () => {
+  const [isNavbarVisible, setNavbarVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(windowPosition);
-
+  const [isFillBackground, setIsFillBackground] = useState(false)
   const handleScroll = () => {
     const currentScrollPos = window.pageYOffset
-    const screenHeight = window.innerHeight
     const isScrollingUp = prevScrollPos > currentScrollPos
-    const isAtScreenBottom = currentScrollPos + screenHeight >= document.body.offsetHeight
-    const isOverlayScreen = isLanding ? (currentScrollPos > screenHeight) : true
 
-    setNavbarVisible((isScrollingUp || isAtScreenBottom) && isOverlayScreen)
+    setIsFillBackground(currentScrollPos > window.innerHeight)
+    setNavbarVisible(isScrollingUp)
     setPrevScrollPos(currentScrollPos)
   };
 
@@ -28,10 +27,10 @@ const Navbar = ({isLanding=false}:{isLanding?:boolean}) => {
   },[prevScrollPos])
 
   return (
-      <header className={`boxWidth ${isNavbarVisible ? "fixed":"absolute"} top-0 left-0 right-0 z-[20000]
-      ${(!isNavbarVisible && isLanding) ? "px-2 py-1" : "px-1 py-2"} text-white`}>
+      <header 
+      className={`boxWidth fixed ${isNavbarVisible ? "top-0" : "-top-20 delay-150"} left-0 right-0 transition-top z-[20000] text-white !px-6 py-2`}>
         <nav 
-        className={`${(!isNavbarVisible && isLanding) ? "p-[12px]": "bg-dark rounded-xl p-[8px]"} flexBetween`}>
+        className={`${isFillBackground ? "bg-dark": "bg-transparent"} transition-background flexBetween rounded-xl  p-[8px]`}>
           <span className="text-2xl font-bold ml-2">
           EuroPark
           </span>
@@ -48,9 +47,36 @@ const Navbar = ({isLanding=false}:{isLanding?:boolean}) => {
             <span>Beli Tiket</span>
             </Link>
           </div>
+
         </nav>
       </header>
   )
 }
 
-export default Navbar
+export default LandingNavbar
+
+      // <motion.header 
+      // initial={{ y:-50 }} 
+      // animate={{ y:0 }}
+      // className={`boxWidth ${isNavbarVisible ? "fixed px-1 py-2":"absolute px-2 py-1"} top-0 left-0 right-0 z-[20000] text-white`}>
+      //   <nav 
+      //   className={`${isNavbarVisible ? "bg-dark rounded-xl p-[8px]": "p-[12px]"} flexBetween  p-[12px]`}>
+      //     <span className="text-2xl font-bold ml-2">
+      //     EuroPark
+      //     </span>
+      //     <div className="flex items-center gap-5">
+      //       <ul className="flexCenter gap-6 items-center font-semibold">
+      //         <li>Home</li>
+      //         <li>Koleksi</li>
+      //         <li>Jadwal</li>
+      //       </ul>
+      //       <Link
+      //       href={`/tickets/buy`}
+      //       className="px-4 py-[6px] font-medium rounded-lg text-black bg-primary"
+      //       >
+      //       <span>Beli Tiket</span>
+      //       </Link>
+      //     </div>
+      //
+      //   </nav>
+      // </motion.header>
--- components/landing/Welcome.tsx
+++ components/landing/Welcome.tsx
@@ -1,27 +1,48 @@
+"use client"
 import Image from "next/image"
+import { motion } from "framer-motion";
 
 const Welcome = () => {
+
+  const headerVariant = {
+    visible: { opacity:1,transition:{staggerChildren:.3} },
+    hidden: {opacity:0}
+  }
+  const titleVarriant = {
+    visible: {opacity:1,y:0},
+    hidden: { opacity:0, y:100}
+  }
   return (
-    <section className="boxWidth min-h-screen flexCenter gap-[70px] my-32 flex-col">
-      <div className="flexBetween gap-2">
-        <div className="basis-[60%] -mt-3">
+    <motion.section className="boxWidth min-h-screen flexCenter gap-16 my-32 flex-col">
+      <motion.div variants={headerVariant} initial="hidden" whileInView={`visible`} viewport={{ once:true, margin:"0% 0% -40% 0%" }} className="flex justify-between gap-10">
+        <motion.div variants={titleVarriant} className="basis-[60%] h-fit">
           <span className="font-bold text-[42px] leading-[52px]">Selamat Datang Di Museum Seni Terbaik di Indonesia</span>
-        </div>
-        <div className="basis-[40%] leading-[28px]">
-          <span className="text-justify block">Europark adalah museum pertama di Indonesia yang memiliki seni lukisan legendaris dan terbaik di dunia. Lebih dari 7 lukisan legendaris yang kami simpan seperti Monalisa, jajang dan dudung</span>
-        </div>
-      </div>
+        </motion.div>
+        <motion.div variants={titleVarriant}  className="basis-[40%] leading-[28px] h-fit">
+          <span className="text-justify text-lg block leading-9">Europark adalah museum pertama di Indonesia yang memiliki seni lukisan legendaris dan terbaik di dunia. Kami juga dsad sadasdbh dsadsahdbshjd dsadsadbs</span>
+        </motion.div>
+      </motion.div>
 
-      <div className="w-full flexCenter gap-5">
-        <div 
+      <motion.div variants={headerVariant} initial="hidden" whileInView={`visible`} viewport={{ once:true, margin:"0% 0% -40% 0%" }} className="w-full flexCenter gap-5">
+        <motion.div 
+        variants={titleVarriant}
         className="bg-[url('/images/about/1.jpg')] background-cover basis-[31.5%] shadow aspect-square"/>
-        <div 
-        className="bg-[url('/images/about/2.jpg')] bg-no-repeat bg-cover bg-center basis-1/3 shadow aspect-square"/>
-        <div 
-        className="bg-[url('/images/about/3.jpg')] backgroud-cover basis-[31.5%] aspect-square shadow"/>
-      </div>
+        <motion.div 
 
-    </section>
+        variants={titleVarriant}
+        className="bg-[url('/images/about/2.jpg')] bg-no-repeat bg-cover bg-center basis-1/3 shadow aspect-square"/>
+        <motion.div 
+        variants={titleVarriant}
+        className="bg-[url('/images/about/3.jpg')] backgroud-cover basis-[31.5%] aspect-square shadow"/>
+      </motion.div>
+
+    </motion.section>
+  )
+}
+
+const ImagesPreview = () => {
+  return (
+    <div>hha</div>
   )
 }
 
